9ae72416e2efc20a0ffc5b856d6bc1d6a9b23a7a
rachidamrani <rachid3alaoui@gmail.com>
2023-12-19 22:02:48 +0100

    first commit

---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,11 @@
+﻿using DiceRollGame;
+
+// Create an instance of Dice Class
+Dice dice = new Dice();
+// Rolling the dice 
+dice.Roll();
+// Start the game
+dice.startGame();
+
+
+Console.ReadKey();
